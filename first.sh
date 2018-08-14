b7ea9497495e39c284ff2fa7f85c18dc0b7666e2
Administrator <admin@example.com>
2018-08-14 19:27:07 +0200

    Add fornodes for other nodes to use

--- first.sh
+++ first.sh
@@ -6,7 +6,7 @@ echo "Please type your project name in gitlab, which is used as service name"
 read PROJECTNAME
 echo $PROJECTNAME>>"$destdir"
 #echo $PROJECTNAME
-docker service create --mode=global  --with-registry-auth --constraint node.role==worker -d --name $PROJECTNAME  --env="DISPLAY" --env QT_X11_NO_MITSHM=1   --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix $(hostname):4567/root/$PROJECTNAME ping google.com
+docker service create --mode=global  --with-registry-auth --constraint node.role==worker -d --name $PROJECTNAME  --env="DISPLAY" --env QT_X11_NO_MITSHM=1   --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix $(hostname):4567/root/$PROJECTNAME bash drawsquare.sh
 
 git add "$destdir"
 git commit -a -m "Add fornodes for other nodes to use"
